5b2548a088cea3f2d310034c3a257d881f548f54
kiseru <kis.alex97@gmail.com>
2018-10-24 16:39:11 +0300

    Fixed matrix multiplication.

--- mpi/part2/task2.cpp
+++ mpi/part2/task2.cpp
@@ -84,72 +84,68 @@ void taskB() {
 
     MPI_Status status;
 
-    int n = 4;
-    int m = 5;
+    int n = 2;
+    int k = 3;
+    int m = 4;
 
     if (rank == 0) {
         printf("Task B\n");
-        int **a = initMatrix(n, m);
-        int **b = initMatrix(m, n);
+        int **a = initMatrix(n, k);
+        int **b = initMatrix(k, m);
 
         printf("A:\n");
-        printMatrix(a, n, m);
+        printMatrix(a, n, k);
         printf("B:\n");
-        printMatrix(b, m, n);
+        printMatrix(b, k, m);
 
-        int **bT = new int *[n];
-        for (int i = 0; i < n; i++) {
-            bT[i] = new int[m];
-            for (int j = 0; j < m; j++) {
+        int **bT = new int *[m];
+        for (int i = 0; i < m; i++) {
+            bT[i] = new int[k];
+            for (int j = 0; j < k; j++) {
                 bT[i][j] = b[j][i];
             }
         }
 
-        printf("BT\n");
-        printMatrix(bT, n, m);
-
         for (int i = 1; i < size; i++) {
-            MPI_Send(a[i - 1], m, MPI_INT, i, 33, MPI_COMM_WORLD);
-            for (int j = 0; j < n; j++) {
-                MPI_Send(bT[j], m, MPI_INT, i, 33, MPI_COMM_WORLD);
+            MPI_Send(a[i - 1], k, MPI_INT, i, 33, MPI_COMM_WORLD);
+            for (int j = 0; j < m; j++) {
+                MPI_Send(bT[j], k, MPI_INT, i, 33, MPI_COMM_WORLD);
             }
         }
 
         int **c = new int *[n];
         for (int i = 0; i < n; i++) {
-            c[i] = new int[n];
+            c[i] = new int[m];
         }
 
         for (int i = 1; i < size; i++) {
-            MPI_Recv(c[i - 1], n, MPI_INT, i, 33, MPI_COMM_WORLD, &status);
+            MPI_Recv(c[i - 1], m, MPI_INT, i, 33, MPI_COMM_WORLD, &status);
         }
 
         printf("C:\n");
-        printMatrix(c, n, n);
+        printMatrix(c, n, m);
     } else {
         MPI_Probe(0, 33, MPI_COMM_WORLD, &status);
 
         int amount;
         MPI_Get_count(&status, MPI_INT, &amount);
 
-        printf("amount = %d\n", amount);
-
         int x[amount];
         MPI_Recv(x, amount, MPI_INT, 0, 33, MPI_COMM_WORLD, &status);
 
-        int result[n];
-        for (int j = 0; j < n; j++) {
+        int result[m];
+        for (int j = 0; j < m; j++) {
             result[j] = 0;
         }
 
-        for (int i = 0; i < n; i++) {
+        for (int i = 0; i < m; i++) {
             int y[amount];
             MPI_Recv(y, amount, MPI_INT, 0, 33, MPI_COMM_WORLD, &status);
             for (int j = 0; j < amount; j++) {
                 result[i] += x[j] * y[j];
             }
         }
-        MPI_Send(result, n, MPI_INT, 0, 33, MPI_COMM_WORLD);
+        MPI_Send(result, m, MPI_INT, 0, 33, MPI_COMM_WORLD);
     }
 }
 
